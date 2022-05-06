274824078c1ffbf7c4931bdf3d6c0072d22aed50
Micaela Korol <84535869+micaelakorol@users.noreply.github.com>
2022-05-05 22:43:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,18 @@
   import Formulario from './components/Formulario'
   import PintarDatos from './components/PintarDatos'
 
-
-  //COMPONENTE PRINCIPAL : 
-
   const App = () => {
 
-  const [nombrePersonaje, setNombrePersonaje] = useState('') //ahora tienen el valor de la prop. nombre (de los input)
+  const [nombrePersonaje, setNombrePersonaje] = useState('')
 
     return (
       <>
-
   <h1>App Rick And Morty</h1>
-  <Formulario setNombrePersonaje={setNombrePersonaje} /> {/*lo enviamos al componente formulario || setNombrePersonaje actualiza el method.*/}
+  <Formulario setNombrePersonaje={setNombrePersonaje} /> 
   <PintarDatos nombrePersonaje={nombrePersonaje} />
-
       </>
     )
   }
 
   export default App
-  //el modificador set, se envia al form , porque es el que muta,va a cambiar ,cada vez que elija otro personaje
-  // y el nombrePersonaje, se envia a PintarDatos, ya que es donde se va a desplegar la informacion del personaje que elegimos
-
-  //Debemos traer de formulario.jsx a app.jsx (componente padre) el nombre/palabra que ingreso el user en el input, (lo que esta buscando)
-  // luego hacerlo viajar a PintarDatos,hacer la solicitud a la api,  para que busque ese personaje en especifico que escribimos en el input.
+ 
