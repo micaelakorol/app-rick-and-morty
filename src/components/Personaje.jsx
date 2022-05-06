6b8455053be93aad4ad53766928326fd8de5dc40
Micaela Korol <84535869+micaelakorol@users.noreply.github.com>
2022-05-05 22:38:35 -0300

    Update Personaje.jsx

--- src/components/Personaje.jsx
+++ src/components/Personaje.jsx
@@ -1,13 +1,8 @@
   import React from 'react'
 
-  //recibe a los item (personajes) del array que recorrimos en PintarDatos:
-  //como anteriormente iteramos con map , ahora hacemos unas card para pintar esos personajes dentro:
   const Personaje = ({personaje}) => {
-  //desestructuramos: nos fijamos en el state, de que manera vienen los datos en este caso, como obj. usamos{}
 
-  const {name , species , image ,gender} = personaje //para no llamar a personaje.name ,etc
-  //caracteristicas qe quiero sacar de cada personaje(tbm aparecen en el status de la consola)
-
+  const {name , species , image ,gender} = personaje
     return (
       <>
 
@@ -15,13 +10,12 @@
 
   <img src={image} alt="" className='card-img-top'/>
   <div className="card-body">
-    <h5>{name}</h5> {/*pintamos esas caracteristicas de forma dinamica que desestructuramos aqui*/}
+    <h5>{name}</h5>
     <p>{species}</p>
     <p>{gender}</p>
   </div>
   </div>
-
-      </>
+ </>
     )
   }
 
